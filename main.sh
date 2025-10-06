c2387680c31a47bcefc5194db4bd569d4a3e46ea
Idalundtorp <70795109+Idalundtorp@users.noreply.github.com>
2025-10-06 14:20:21 +0200

    Update main.sh
    Removed path specific comments in code

--- main.sh
+++ main.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-# For an overview of production and for further information see the confluence page called
-# SIC (DMI_MSC_SIC) overview of production
 
 # ========================
 # INPUTS
@@ -28,7 +26,6 @@ echo "Running pipeline from $START_DATE to $END_DATE"
 # ========================
 # STEP 1: DOWNLOAD RELEVANT DATA
 # ========================
-# CDS data is transferred from AURORA including: OSI-458-TU, FMI/SMHI icecharts, BALTIC SST, and ESACCI SST/OSTIA SST
 # Download NIC icecharts
 
 # Shift dates - create extended ranges to ensure that we get an icechart (updated biweekly)
@@ -36,7 +33,7 @@ START_EXT=$(date -d "$START_DATE -14 days" +%Y-%m-%d)
 END_EXT=$(date -d "$END_DATE +14 days" +%Y-%m-%d)
 
 echo "Downloading NIC data"
-#python download_nic.py "$START_EXT" "$END_EXT" "$DATA_BASEPATH/original_SIC_data/NIC/shapefiles" #: this script downloads the data between two given dates and unzips the downloaded data, path=download path
+python download_nic.py "$START_EXT" "$END_EXT" "$DATA_BASEPATH/original_SIC_data/NIC/shapefiles" #: this script downloads the data between two given dates and unzips the downloaded data, path=download path
 python Read_NIC_shapefiles.py "$START_EXT" "$END_EXT" "$DATA_BASEPATH/original_SIC_data/NIC" # reprojects NIC shapefiles from .sig file to an .nc file in EASE3 grid
 
 # ========================
@@ -44,8 +41,8 @@ python Read_NIC_shapefiles.py "$START_EXT" "$END_EXT" "$DATA_BASEPATH/original_S
 # ========================
 echo "Regridding data"
 # regrids all of the listed datasources
-#./regrid_data_updated.sh "$START_DATE" "$END_DATE" "OSI458_TU OSTIA BALTIC_SIC FMI_SMHI NIC" "nh" #"OSI458_TU OSTIA BALTIC_SIC FMI_SMHI NIC" "nh" # modify script to take start date and end date and HS
-#./regrid_data_updated.sh "$START_DATE" "$END_DATE" "OSI458_TU OSTIA" "sh" # modify script to take start date and end date and HS
+./regrid_data_updated.sh "$START_DATE" "$END_DATE" "OSI458_TU OSTIA BALTIC_SIC FMI_SMHI NIC" "nh" #"OSI458_TU OSTIA BALTIC_SIC FMI_SMHI NIC" "nh" # modify script to take start date and end date and HS
+./regrid_data_updated.sh "$START_DATE" "$END_DATE" "OSI458_TU OSTIA" "sh" # modify script to take start date and end date and HS
 
 # ========================
 # STEP 3: RUN PRODUCTION OF DMI-MSC-SIC
@@ -53,7 +50,7 @@ echo "Regridding data"
 echo "Run processing of DMI-MSC-SIC"
 # Runs processing chain for each HS
 python "$CODE_BASEPATH/Running_filtering_function_updated.py" "$START_DATE" "$END_DATE" "nh" # modify to take start_date, end date and HS
-#python $COSE_BASEPATH/Running_filtering_function_updated.py START_DATE END_DATE SH
+python $COSE_BASEPATH/Running_filtering_function_updated.py START_DATE END_DATE SH
 
 # ========================
 # STEP 4: COMBINE DMI-MSC-SIC NH AND SH
